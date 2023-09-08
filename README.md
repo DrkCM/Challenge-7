027924efb5a11a2c8bbe14db687ca999cfaeede0
DrkCM <139226423+DrkCM@users.noreply.github.com>
2023-09-08 20:34:20 +0100

    uploading the files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Challenge-7
 Module-07 challenge
+
+A simple calendar app for scheduling your work day
+![Alt text](1.png)
+![Alt text](2.png)
+![Alt text](3.png)
+
+This app has been createn using js third-party APIs!
+
+Deployed link https://drkcm.github.io/Challenge-7/
+
+Repo link https://github.com/DrkCM/Challenge-7
---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    // listen for save button clicks
+
+    $(".saveButton").on("click", function(){
+       console.log(`It saved`);
+
+       let timeID = $(this).parent().attr("id")
+       let value = $(this).siblings(".description").val();
+
+       console.log(timeID);
+       console.log(value);
+
+       localStorage.setItem(timeID, value);
+
+       $(".notification").addClass("show");
+
+
+       setTimeout(function(){
+         $(".notification").removeClass("show");
+       }, 5000)
+
+    })
+
+     function hourUpdate() {
+        let currentHour = moment().hour();
+        console.log(currentHour);
+
+    for(let i = 0; i < $(".time-block").length; i++){
+        let hour = parseInt($(".time-block") [i].getAttribute("id").split("-") [1])
+
+        console.log(hour);
+        console.log(currentHour);
+
+        if(hour < currentHour) {
+            $(".time-block") [i].classList.add("past")
+        } else if(hour === currentHour){
+            $(".time-block") [i].classList.remove("past")
+            $(".time-block") [i].classList.add("present")
+        } else {
+            $(".time-block") [i].classList.remove("past")
+            $(".time-block") [i].classList.remove("present")
+            $(".time-block") [i].classList.add("future")
+        }
+    }
+
+
+    }
+
+     hourUpdate();
+
+     let interval = setInterval(hourUpdate, 15000);
+
+     $("#hour-0 .description").val(localStorage.getItem("hour-0"))
+     $("#hour-1 .description").val(localStorage.getItem("hour-1"))
+     $("#hour-2 .description").val(localStorage.getItem("hour-2"))
+     $("#hour-3 .description").val(localStorage.getItem("hour-3"))
+     $("#hour-4 .description").val(localStorage.getItem("hour-4"))
+     $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+     $("#hour-6 .description").val(localStorage.getItem("hour-6"))
+     $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+     $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+     $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+     $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+     $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+     $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+     $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+     $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+     $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+     $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+     $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+     $("#hour-18 .description").val(localStorage.getItem("hour-18"))
+     $("#hour-19 .description").val(localStorage.getItem("hour-19"))
+     $("#hour-20 .description").val(localStorage.getItem("hour-20"))
+     $("#hour-21 .description").val(localStorage.getItem("hour-21"))
+     $("#hour-22 .description").val(localStorage.getItem("hour-22"))
+     $("#hour-23 .description").val(localStorage.getItem("hour-23"))
+
+
+     $("currentDay").text(moment().format("dddd, MMMM, Do"))
+
+})
